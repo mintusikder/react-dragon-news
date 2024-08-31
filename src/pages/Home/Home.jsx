bf8a76c057d879eef3ba55739fa1698a1d6b23cb
Mintu Sikder <mintusikder15@gmail.com>
2024-08-31 11:17:57 +0600

    added moment format

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,27 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import NavBar from "../Shared/NavBar/NavBar";
-
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
-    return (
+  return (
+    <div>
+      <Header></Header>
+      <NavBar></NavBar>
+      <h2 className="text-3xl font-poppins font-bold">This is home</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
         <div>
-            <Header></Header>
-            <NavBar></NavBar>
-            <h2 className="text-3xl font-poppins font-bold">This is home</h2>
+          <LeftSideNav></LeftSideNav>
         </div>
-    );
+        <div className="col-span-2">
+          <h3>News Coming</h3>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
